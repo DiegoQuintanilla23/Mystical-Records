5a6fca06485a62bc2adb91f30cafb34225dc7498
Diego Quintanilla <diego.quintanilla1@hotmail.com>
2024-03-26 01:55:32 -0600

    Añadiendo paginas

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,14 @@ module.exports = {
     "./src/**/*.{html,ts}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        
+      },
+      fontFamily: {
+        sans: ['Arial', 'sans-serif'],
+      },
+    },
   },
   plugins: [],
 }
