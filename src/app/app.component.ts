3a428821ef2428f7a5eb3e84e714b52cb94285f8
Diego Quintanilla <diego.quintanilla1@hotmail.com>
2024-04-02 00:09:43 -0600

    Feat: Login & Register forms Fixed: Router Outlet

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,28 @@
 import { Component } from '@angular/core';
-import { ActivatedRoute, RouterOutlet } from '@angular/router';
+import { ActivatedRoute, RouterOutlet, Router, NavigationEnd } from '@angular/router';
 import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
+import { NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,HeaderComponent, FooterComponent, NavbarComponent],
+  imports: [NgIf, RouterOutlet,HeaderComponent, FooterComponent, NavbarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'Mystical-Records';
+  public CurrentRoute : string = "";
+
+  constructor(private router: Router) {
+    this.router.events.subscribe(event => {
+      if (event instanceof NavigationEnd) {
+        this.CurrentRoute = event.url;
+        console.log(this.CurrentRoute);
+      }
+    });
+  }
+  
 }
