82aaaf43fe34e6f33de0a58e1430a7af2ae5e5f3
Diego Quintanilla <diego.quintanilla1@hotmail.com>
2024-04-08 17:56:04 -0600

    revision

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -10,6 +10,11 @@ import { OrdersPage } from './pages/user/orders/orders.page';
 import { CommentsPage } from './pages/user/comments/comments.page';
 import { WishlistPage } from './pages/user/wishlist/wishlist.page';
 import { ShoppingCartPage } from './pages/user/shopping-cart/shopping-cart.page';
+import { AddProductPage } from './pages/admin/add-product/add-product.page';
+import { ViewProductsPage } from './pages/admin/view-products/view-products.page';
+import { ViewOrdersPage } from './pages/admin/view-orders/view-orders.page';
+import { ViewClassPage } from './pages/admin/view-class/view-class.page';
+import { NewsletterPage } from './pages/admin/newsletter/newsletter.page';
 
 export const routes: Routes = [
     {path: "home", component: HomePage },
@@ -30,7 +35,12 @@ export const routes: Routes = [
     },
     {path: "dashboard/admin", component: DashboardPage,
         children: [
-
+            {path: '', redirectTo:'home-adm', pathMatch:'full'},
+            {path: "add-product", component: AddProductPage},
+            {path: "view-products", component: ViewProductsPage},
+            {path: "view-orders", component: ViewOrdersPage},
+            {path: "view-class", component: ViewClassPage},
+            {path: "Newsletter", component: NewsletterPage},
         ]
     },
     {path: "", redirectTo:"home", pathMatch:"full"},
