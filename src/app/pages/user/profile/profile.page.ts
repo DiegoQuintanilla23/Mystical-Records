eff49660c7ef624c870c96211380704ea03574f6
Diego Quintanilla <diego.quintanilla1@hotmail.com>
2024-04-29 18:57:05 -0600

    User crud

--- src/app/pages/user/profile/profile.page.ts
+++ src/app/pages/user/profile/profile.page.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { UserFormComponent } from '../../../components/user/user-form/user-form.component';
 
 @Component({
   selector: 'app-profile',
   standalone: true,
-  imports: [],
+  imports: [UserFormComponent],
   templateUrl: './profile.page.html',
   styleUrl: './profile.page.css'
 })
